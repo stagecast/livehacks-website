f75b483bbe9ca1298aa549ad5dfb0a2decdb3d40
Raghu Nayyar <hey@raghunayyar.com>
2017-03-21 22:12:29 +0100

    Everything apart from read more.

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,15 +3,6 @@ $(document).ready(function(){
         $(this).toggleClass('open');
     });
 
-    function openNav() {
-        document.getElementById("myNav").style.width = "100%";
-    }
-
-    /* Close when someone clicks on the "x" symbol inside the overlay */
-    function closeNav() {
-        document.getElementById("myNav").style.width = "0%";
-    }
-
     $('#nav-icon1').click(function() {
 
         if (!$(this).children().hasClass('white')) {
@@ -27,4 +18,28 @@ $(document).ready(function(){
         $('#nav-icon1').children().removeClass('white');
         $('#myNav').removeClass('open');
     })
+
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+    // Add smooth scrolling on all links inside the navbar
+    $(".navbar-list li a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
 });
